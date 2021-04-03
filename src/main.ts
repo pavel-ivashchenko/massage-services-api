3fbc54ca2a914ee5888d57ad137dfc346fc0886b
Pavel Ivashchenko <pivashchenko@greentube.com>
2021-04-03 17:26:06 +0200

    Config: implemented

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
+  
   const logger = new Logger();
-  const port = 3000;
+  const port = process.env.PORT || serverConfig.port;
   
   const app = await NestFactory.create(AppModule);
+  
   await app.listen(port);
   logger.log(`Application listens to port ${port}`);
 }
